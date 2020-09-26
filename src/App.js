4ca51c132946194f063fb64336c59dbdb7b7b6fc
Alejandro Perez <alejandro.perez@pereza.local>
2020-09-26 15:16:17 +0200

    hi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Counter from "./component";
+
+export const ThemeContext = React.createContext();
 
 function App() {
+  console.log("render app");
+  const [theme, setTheme] = useState("red");
+
+  function changeTheme() {
+    setTheme((previousTheme) => (previousTheme === "red" ? "blue" : "red"));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={theme}>
+      <Counter />
+      <button onClick={changeTheme}>Let's change Background</button>
+    </ThemeContext.Provider>
   );
 }
 
